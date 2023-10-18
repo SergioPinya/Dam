177d99a53c4cfe3013b53c9e7aae1b4d7606652b
SergioPinya <msegio2003@gmail.com>
2023-10-18 16:11:20 +0200

    datosEjer1.8

--- servicios/eclipse-workspace/Ejercicio/src/ejerT1/Ejer8.java
+++ servicios/eclipse-workspace/Ejercicio/src/ejerT1/Ejer8.java
@@ -1,12 +1,20 @@
 package ejerT1;
 
+import java.io.File;
+
 public class Ejer8 {
 
 	public static void main(String[] args) {
 		
-		File archivo=new File(args[0]);
-		
-
+		File directorio = new File(args[0]);
+		String[] listaArchivos=null;
+		for (int i = 1; i < args.length; i++) {
+			 listaArchivos = directorio.list(new FiltroExtension(args[i]));
+			 for (int e = 0; e < listaArchivos.length; e++) {
+					System.out.println(listaArchivos[e]);
+				}
+			
+		}
 	}
 
 }

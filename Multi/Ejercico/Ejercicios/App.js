a4ef426fc0caf72ce91692ece39ff2f85aa888e8
SergioPinya <msegio2003@gmail.com>
2023-10-24 16:19:45 +0200

    Ejercicio2.5

--- Multi/Ejercico/Ejercicios/App.js
+++ Multi/Ejercico/Ejercicios/App.js
@@ -1,5 +1,5 @@
 import Ejer1 from './Ejer/Ejer1'
-
+import Ejer2 from './Ejer/Ejer2'
 export default function App(){
-  return <Ejer1/>
+  return <Ejer2/>
 }
--- Multi/Ejercico/Ejercicios/Ejer/Ejer2.js
+++ Multi/Ejercico/Ejercicios/Ejer/Ejer2.js
@@ -1,65 +1,76 @@
 import React from "react";
 import {
-  SafeAreaView,
-  StyleSheet,
-  Alert,
-  Button,
-  TextInput,
-  Text,
+    SafeAreaView,
+    StyleSheet,
+    Alert,
+    Button,
+    TextInput,
+    Text,
 } from "react-native";
 import { useState } from 'react';
 
 
-const Ejer1 = () => {
-  const [text, setText] = useState("");
-  const [numeros, setNumeros]=useState("");
+const Ejer2 = () => {
+    const [text, setText] = useState("");
+    const [numeros, setNumeros] = useState([]);
+
+    function al() {
+
+        setNumeros(numeros + text + "\n")
+        setText("");
+    }
     function alerta() {
 
-      if (isNaN(text)) {
-        setText('');
-        alert('Has introducido texto');
-      } else if (text == "") {
-        alert('No has introducido nada');
-      } else if (!isNaN(text)) {
-        alert('Has introducido un número');
-        setNumeros(text)
-        setText('');
-       
-      }
-       
+        if (isNaN(text)) {
+            setText('');
+            alert('Has introducido texto');
+        } else if (text == "") {
+            alert('No has introducido nada');
+        } else if (!isNaN(text)) {
+            alert('Has introducido un número');
+
+            al()
+
+
+        }
+
     }
 
-  return (
-    <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={newText => setText(newText)}
-        placeholder="useless placeholder"
-        value={text}
-      />
-        <Button 
-        style={styles.buto}
-        title="Press me"
-        onPress={() => alerta()}
-      />
-      <Text>{numeros}</Text>
-    </SafeAreaView>
-  );
+
+    return (
+        <SafeAreaView>
+            <TextInput
+                style={styles.input}
+                onChangeText={newText => setText(newText)}
+                placeholder="useless placeholder"
+                value={text}
+            />
+            <Button
+                style={styles.buto}
+                title="Press me"
+                onPress={() => alerta()}
+            />
+            <Text style={styles.resul}>{numeros}</Text>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    marginTop: 80,
-  },
-  buto:{
-    height: 20,
+    input: {
+        height: 40,
+        margin: 12,
 
+        padding: 10,
+        marginTop: 80,
+        textAlign: "center",
+    },
+    buto: {
+        width: 5,
 
-  },
+    },
+    resul: {
+        textAlign: "center",
+    },
 });
 
-export default Ejer1;
+export default Ejer2;

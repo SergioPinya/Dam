0436f079b141a7b4cdf9a5b9859d4561316a3345
SergioPinya <msegio2003@gmail.com>
2023-10-27 19:12:27 +0200

    seviciosEjercicios

---
+++ servicios/Ejercicios/t2/src/ejercicios/Ejer1.java
@@ -0,0 +1,12 @@
+package ejercicios;
+
+public class Ejer1 {
+
+	public static void main(String[] args) {
+		//Realitza un programa en Java que donats dos números enters, torne per pantalla la suma de
+		//tots els números que hi ha entre ells (incloent-los).
+		
+		
+	}
+
+}

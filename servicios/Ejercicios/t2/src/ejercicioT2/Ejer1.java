353d225927ca7dbc70406db848291be8ae548f01
SergioPinya <msegio2003@gmail.com>
2023-10-27 19:24:49 +0200

    Ejercicio1t2

---
+++ servicios/Ejercicios/t2/src/ejercicioT2/Ejer1.java
@@ -0,0 +1,35 @@
+package ejercicioT2;
+
+public class Ejer1 {
+
+	public static int sumar(int n1, int n2) {
+
+		System.out.println("Sumando de " + n1 + " hasta " + n2);
+
+		int resultado = 0;
+
+		for (int i = n1; i <= n2; i++) {
+
+			resultado = resultado + i;
+
+		}
+
+		return resultado;
+
+	}
+
+	public static void main(String[] args) {
+
+		Ejer1 s = new Ejer1();
+
+		int n1 = Integer.parseInt(args[0]);
+
+		int n2 = Integer.parseInt(args[1]);
+
+		int resultado = Ejer1.sumar(n1, n2);
+
+		System.out.println("Resultado de este sumador: " + resultado);
+
+	}
+
+}
---
+++ servicios/Ejercicios/t2/src/ejercicioT2/Lanzador.java
@@ -0,0 +1,81 @@
+package ejercicioT2;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public  class Lanzador {
+
+	public static void lanzarSumador(Integer n1,Integer n2){
+
+	 
+
+	try{
+
+	String clase="ejercicioT2.Ejer1";
+	//El comando de abajo es para localizar la maquina de java en cualquier maquina
+	String javaHome=System.getProperty("java.home");
+	String javaBin=javaHome+File.separator+"bin"+File.separator+"java";
+	String classpath=System.getProperty("java.class.path");
+	//
+	String className=clase;
+
+	 
+
+	List<String>command=new ArrayList<>();
+
+	command.add(javaBin);
+
+	command.add("-cp");
+	command.add(classpath);
+	command.add(className);
+	command.add(n1.toString());
+	command.add(n2.toString());
+
+	 
+
+	System.out.println("Comando que se pasa a ProcessBuilder: "+command);
+
+	System.out.println("Comando a ejecutar en cmd.exe: "+command.toString().replace(",",""));
+
+	 
+
+	ProcessBuilder builder=new ProcessBuilder(command);
+// el comando de abajo es el que inicia todo
+	Process process=builder.inheritIO().start();
+	
+	
+//opcional{
+	// Process process = builder.inheritIO().start();
+
+	// Process process = builder.redirectOutput(new File("temp.txt")).start();
+//}
+	 
+// dice codigo de error (si es 0 es que ha ido todo bien)
+	process.waitFor();
+
+	System.out.println(process.exitValue());
+//
+	 
+
+	}catch( Exception e ){
+
+	e.printStackTrace();
+
+	}
+
+	}
+
+	public static void main(String[] args) {
+
+		Lanzador l = new Lanzador();
+
+		lanzarSumador(1, 50);
+
+		lanzarSumador(51, 100);
+
+		System.out.println("Ok");
+
+	}
+
+}

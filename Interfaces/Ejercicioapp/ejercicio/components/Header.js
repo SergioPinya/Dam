e764b236f9b2c5d23e2c0f05b15fd7eb43369cef
SergioPinya <msegio2003@gmail.com>
2023-10-30 18:37:00 +0100

    StilosFlexInter

--- Interfaces/Ejercicioapp/ejercicio/components/Header.js
+++ Interfaces/Ejercicioapp/ejercicio/components/Header.js
@@ -37,4 +37,75 @@ const Header = (Props) => {
     </View>
     )
   }
+  const styles = StyleSheet.create({
+    contenidor: {
+      flex: 1,
+      flexDirection: 'column',
+     
+    },
+    contenidor2: {
+      flex: 1,
+      flexDirection: 'column',
+    },
+    contHeaderImg: {
+      flex: 0.5,
+      flexDirection: 'column',
+    },
+    headerImg: {
+      width: 100,
+      height: 100,
+      borderRadius: 100,
+      borderColor: "black",
+      borderWidth: 2
+    },
+    headerCont2: {
+      flex: 1,
+      flexDirection: 'row'
+    },
+  
+    contHeader: {
+      flex: 1,
+    }, 
+    contenidor4: {
+      flex: 1
+    },
+  
+    seccio1: {
+      flex: 0.25,
+      fontSize: 12,
+      fontWeight: '600',
+      margin: 4,
+      padding: 12,
+      textAlign: 'right',
+      flexDirection: 'row',
+    },
+    seccio2: {
+      flex: 1,
+     
+     
+      fontSize: 12,
+      fontWeight: '600',
+      margin: 4,
+      padding: 12,
+      textAlign: 'center',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      flexWrap: "wrap"
+  
+    },
+  
+    imagen: {
+      width: 80,
+      height: 80,
+      marginTop: 10
+    },
+    seccio3: {
+      flex: 0.25,
+      fontSize: 12,
+      fontWeight: '600',
+      margin: 4,
+      padding: 12,
+      textAlign: 'left',
+    },
+  });
   export default Header;

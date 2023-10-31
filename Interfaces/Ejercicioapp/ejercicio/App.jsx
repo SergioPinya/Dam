6b79e2ae4d119b08bfe586658c8d12bfae164ce9
SergioPinya <msegio2003@gmail.com>
2023-10-31 09:13:11 +0100

    calcuIMCINterfaz

--- Interfaces/Ejercicioapp/ejercicio/App.jsx
+++ Interfaces/Ejercicioapp/ejercicio/App.jsx
@@ -6,24 +6,92 @@
  */
 
 import React from 'react';
-import { Alert, ScrollView, StatusBar,StyleSheet,  View } from 'react-native';
+import { Alert, ScrollView, StatusBar, StyleSheet, View } from 'react-native';
 import { Appearance } from 'react-native';
+import { useState } from 'react';
 import { TextInput } from 'react-native-paper';
 import { Button } from 'react-native-paper';
 import { Switch } from 'react-native-paper';
 import { Provider as PaperProvider, Text } from 'react-native-paper';
- //pruS
+
+
 
 const App = () => {
+  const [peso, setPeso]=useState(0);
+  const [alçada, setAlçada]=useState(0);
+  const [resultat, setResultat]=useState(0);
+  const [imc, setImc]=useState(0);
+const calcular= () => {
+  setResultat(peso/Math.pow(alçada,2));
+  if (resultat<18.5) {
+    setImc("pu")
+  }
+  
+};
   return (
-    ""
+    <PaperProvider>
+      <View style={styles.contenidor}>
+        <View style={styles.header}>
+          <Text>Calcular pes</Text>
+        </View>
+        <View style={styles.articlepes}>
+          <Text>Pes</Text><TextInput onChangeText={text=>setPeso(text)}></TextInput>
+        </View>
+        <View style={styles.articlealt}>
+          <Text>Alçada</Text><TextInput onChangeText={text=>setAlçada(text)}></TextInput>
+        </View>
+        <View style={styles.articlecal}>
+          <Button mode="contained" onPress={() => calcular()}>
+            Calcular
+          </Button>
+        </View>
+        <View style={styles.articleres}>
+          <Text>Tu IMC es {resultat}{imc}</Text>
+        </View>
+      </View>
+    </PaperProvider>
   );
 };
 const styles = StyleSheet.create({
- 
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  contenidor: {
+    flex: 1,
+    padding: 1,
+   
+    flexDirection: 'column',
+   
   },
-  })
+  header: {
+    borderColor: "red",
+    borderWidth: 2,
+    flex: 1,
+    padding: 1,
+
+  },
+  articlepes: {
+    borderColor: "blue",
+    borderWidth: 2,
+    flex: 2,
+    padding: 1,
+  },
+  articlealt: {
+    borderColor: "green",
+    borderWidth: 2,
+    flex: 2,
+    padding: 1,
+  },
+  articlecal: {
+    borderColor: "orange",
+    borderWidth: 1,
+    flex: 1,
+    padding: 1,
+    justifyContent:"center"
+  },
+  articleres: {
+    borderColor: "brown",
+    borderWidth: 2,
+    flex: 2,
+    padding: 1,
+  }
+});
+
 export default App;

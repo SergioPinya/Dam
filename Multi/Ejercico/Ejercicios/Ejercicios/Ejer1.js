5b4a7b4592b74c1e5bbc1a8e0fafc9f869aeb679
SergioPinya <msegio2003@gmail.com>
2023-11-14 16:23:51 +0100

    Limpieza3

---
+++ Multi/Ejercico/Ejercicios/Ejercicios/Ejer1.js
@@ -0,0 +1,18 @@
+import { TextInput, Text, ScrollView, View } from "react-native";
+import { useState } from "react";
+
+
+const Ejer1 = () =>{ 
+return (
+    <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+    <Text>Try editing me! 🎉</Text>
+  </View>
+);
+};
+
+export default Ejer1;
